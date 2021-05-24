7ef881e49ed5fdd91773f2788dd1ea6822b72f94
HyungsooLim <hyungsoolim0@gmail.com>
2021-05-24 16:44:40 +0900

    Initial commit

---
+++ src/main/java/com/hs/s1/FwccApplication.java
@@ -0,0 +1,13 @@
+package com.hs.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwccApplication.class, args);
+	}
+
+}
