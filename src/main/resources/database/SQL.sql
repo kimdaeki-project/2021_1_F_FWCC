a9873a53865c2ad9a75174d2ae63b2881ff5e28c
rihee1214 <rihee1214@gmail.com>
2021-05-26 16:28:46 +0900

    Update SQL.sql

--- src/main/resources/database/SQL.sql
+++ src/main/resources/database/SQL.sql
@@ -208,11 +208,11 @@ values(0, 'username3', 'addrName3', 'recipient3', 'addrPhone3', 'zipCode3', 'bas
 =====================================이리희=======================================================
 ---------------------------------------------------------- create table cart ---------------------------------------------------------------------
 CREATE TABLE `fw01`.`cart` (
-	`cartNum` BIGINT(20) NOT NULL AUTO_INCREMENT,
-	`productNum` BIGINT(20) NOT NULL,
-	`pInfoNum` BIGINT(20) NOT NULL,
-	`productCount` BIGINT(20) NOT NULL,
-	`username` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	`cartNum` BIGINT NOT NULL AUTO_INCREMENT,
+	`productNum` BIGINT NOT NULL,
+	`pInfoNum` BIGINT NOT NULL,
+	`productCount` BIGINT NOT NULL,
+	`username` VARCHAR(100) NOT NULL,
 	PRIMARY KEY (`cartNum`) USING BTREE,
 	INDEX `CT_PN_FK` (`productNum`) USING BTREE,
 	INDEX `CT_PI_FK` (`pInfoNum`) USING BTREE,
@@ -221,8 +221,6 @@ CREATE TABLE `fw01`.`cart` (
 	CONSTRAINT `CT_PN_FK` FOREIGN KEY (`productNum`) REFERENCES `fw01`.`product` (`productNum`) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT `CT_UN_FK` FOREIGN KEY (`username`) REFERENCES `fw01`.`member` (`username`) ON UPDATE CASCADE ON DELETE CASCADE
 )
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
 ;
 
 ------------------------------------- cart 더미데이터 -----------------------------------------------
@@ -242,18 +240,16 @@ VALUES(0, 3, 10, 1, 'admin');
 ---------------------------------------------------------- create table coupon ---------------------------------------------------------------------
 
 CREATE TABLE `fw01`.`coupon` (
-	`cuNum` BIGINT(20) NOT NULL AUTO_INCREMENT,
-	`username` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
-	`disRate` INT(11) NOT NULL,
+	`cuNum` BIGINT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(100) NOT NULL,
+	`disRate` INT NOT NULL,
 	`pubDate` DATE NOT NULL,
 	`exDate` DATE NOT NULL,
-	`cuName` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	`cuName` VARCHAR(100) NOT NULL,
 	PRIMARY KEY (`cuNum`) USING BTREE,
 	INDEX `CU_UN_FK` (`username`) USING BTREE,
 	CONSTRAINT `CU_UN_FK` FOREIGN KEY (`username`) REFERENCES `fw01`.`member` (`username`) ON UPDATE CASCADE ON DELETE CASCADE
 )
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
 ;
 
 ------------------------------------- coupon 더미데이터 -----------------------------------------------
@@ -272,38 +268,37 @@ VALUES(0, 'admin', 40, NOW(), NOW(), 'test4');
 ---------------------------------------------------------- create table orderlist ---------------------------------------------------------------------
 
 CREATE TABLE `fw01`.`orderlist` (
-	`orderNum` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
-	`totPrice` BIGINT(20) NOT NULL,
-	`spPrice` BIGINT(20) NOT NULL,
-	`cuNum` BIGINT(20) NOT NULL,
-	`username` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
-	`destination` VARCHAR(450) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+	`orderNum` VARCHAR(100) NOT NULL,
+	`totPrice` BIGINT NOT NULL,
+	`spPrice` BIGINT NOT NULL,
+	`cuNum` BIGINT NOT NULL,
+	`username` VARCHAR(100) NOT NULL,
+	`destination` VARCHAR(450) NOT NULL DEFAULT '',
+	`orderDate` DATETIME NOT NULL,
 	PRIMARY KEY (`orderNum`) USING BTREE,
 	INDEX `OD_CN_FK` (`cuNum`) USING BTREE,
 	INDEX `OD_UN_FK` (`username`) USING BTREE,
 	CONSTRAINT `OD_CN_FK` FOREIGN KEY (`cuNum`) REFERENCES `fw01`.`coupon` (`cuNum`) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT `OD_UN_FK` FOREIGN KEY (`username`) REFERENCES `fw01`.`member` (`username`) ON UPDATE CASCADE ON DELETE CASCADE
 )
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
 ;
 
 ------------------------------------- orderlist 더미데이터 -----------------------------------------------
 
-INSERT INTO orderlist (orderNum, totPrice, spPrice, cuNum, username, destination)
-VALUES('123-123', 100000, 100000, 1, 'admin', 'test1');
-INSERT INTO orderlist (orderNum, totPrice, spPrice, cuNum, username, destination)
-VALUES('234-234', 10000, 9000, 1, 'admin', 'test2');
+INSERT INTO orderlist (orderNum, totPrice, spPrice, cuNum, username, destination, orderDate)
+VALUES('123-123', 100000, 100000, 1, 'admin', 'test1', sysdate());
+INSERT INTO orderlist (orderNum, totPrice, spPrice, cuNum, username, destination, orderDate)
+VALUES('234-234', 10000, 9000, 1, 'admin', 'test2', sysdate());
 
 ---------------------------------------------------------- create table purchase ---------------------------------------------------------------------
 
 CREATE TABLE `fw01`.`purchase` (
-	`purNum` BIGINT(20) NOT NULL AUTO_INCREMENT,
-	`productNum` BIGINT(20) NOT NULL,
-	`pInfoNum` BIGINT(20) NOT NULL,
-	`orderNum` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
-	`productCount` INT(11) NOT NULL DEFAULT '0',
-	`proPriceSum` BIGINT(20) NOT NULL,
+	`purNum` BIGINT NOT NULL AUTO_INCREMENT,
+	`productNum` BIGINT NOT NULL,
+	`pInfoNum` BIGINT NOT NULL,
+	`orderNum` VARCHAR(100) NOT NULL,
+	`productCount` INT NOT NULL DEFAULT '0',
+	`proPriceSum` BIGINT NOT NULL,
 	PRIMARY KEY (`purNum`) USING BTREE,
 	INDEX `PUR_PI_FK` (`pInfoNum`) USING BTREE,
 	INDEX `PUR_PN_FK` (`productNum`) USING BTREE,
@@ -312,8 +307,6 @@ CREATE TABLE `fw01`.`purchase` (
 	CONSTRAINT `PUR_PI_FK` FOREIGN KEY (`pInfoNum`) REFERENCES `fw01`.`productinfo` (`pInfoNum`) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT `PUR_PN_FK` FOREIGN KEY (`productNum`) REFERENCES `fw01`.`product` (`productNum`) ON UPDATE CASCADE ON DELETE CASCADE
 )
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
 ;
 
 ------------------------------------- purchase 더미데이터 -----------------------------------------------
@@ -330,21 +323,19 @@ VALUES(0, 1, 2, '234-234', 1, 10000);
 ---------------------------------------------------------- create table mileage ---------------------------------------------------------------------
 
 CREATE TABLE `fw01`.`mileage` (
-	`mileNum` BIGINT(20) NOT NULL AUTO_INCREMENT,
-	`usedMile` BIGINT(20) NOT NULL,
-	`unableMile` BIGINT(20) NOT NULL,
-	`username` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
-	`orderNum` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
-	`mileContents` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
-	`enabledMile` BIGINT(20) NOT NULL,
+	`mileNum` BIGINT NOT NULL AUTO_INCREMENT,
+	`usedMile` BIGINT NOT NULL,
+	`unableMile` BIGINT NOT NULL,
+	`username` VARCHAR(100) NOT NULL,
+	`orderNum` VARCHAR(100) NOT NULL,
+	`mileContents` VARCHAR(100) NOT NULL,
+	`enabledMile` BIGINT NOT NULL,
 	PRIMARY KEY (`mileNum`) USING BTREE,
 	INDEX `MI_UN_FK` (`username`) USING BTREE,
 	INDEX `MI_ON_FK` (`orderNum`) USING BTREE,
 	CONSTRAINT `MI_ON_FK` FOREIGN KEY (`orderNum`) REFERENCES `fw01`.`orderlist` (`orderNum`) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT `MI_UN_FK` FOREIGN KEY (`username`) REFERENCES `fw01`.`member` (`username`) ON UPDATE CASCADE ON DELETE CASCADE
 )
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
 ;
 
 ------------------------------------- mileage 더미데이터 -----------------------------------------------
