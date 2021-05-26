5748bfc5ca0b154e3c718e63c92a702e8e691fb1
rihee1214 <rihee1214@gmail.com>
2021-05-26 15:38:34 +0900

    Update SQL.sql

--- src/main/resources/database/SQL.sql
+++ src/main/resources/database/SQL.sql
@@ -203,3 +203,153 @@ insert into address(addrNum, username, addrName, recipient, addrPhone, zipCode, 
 values(0, 'username3', 'addrName2', 'recipient2', 'addrPhone2', 'zipCode2', 'basicAddr2', 'detailAddr2');
 insert into address(addrNum, username, addrName, recipient, addrPhone, zipCode, basicAddr, detailAddr)
 values(0, 'username3', 'addrName3', 'recipient3', 'addrPhone3', 'zipCode3', 'basicAddr3', 'detailAddr3');
+
+
+=====================================이리희=======================================================
+---------------------------------------------------------- create table cart ---------------------------------------------------------------------
+CREATE TABLE `fw01`.`cart` (
+	`cartNum` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`productNum` BIGINT(20) NOT NULL,
+	`pInfoNum` BIGINT(20) NOT NULL,
+	`productCount` BIGINT(20) NOT NULL,
+	`username` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`cartNum`) USING BTREE,
+	INDEX `CT_PN_FK` (`productNum`) USING BTREE,
+	INDEX `CT_PI_FK` (`pInfoNum`) USING BTREE,
+	INDEX `CT_UN_FK` (`username`) USING BTREE,
+	CONSTRAINT `CT_PI_FK` FOREIGN KEY (`pInfoNum`) REFERENCES `fw01`.`productinfo` (`pInfoNum`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `CT_PN_FK` FOREIGN KEY (`productNum`) REFERENCES `fw01`.`product` (`productNum`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `CT_UN_FK` FOREIGN KEY (`username`) REFERENCES `fw01`.`member` (`username`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+------------------------------------- cart 더미데이터 -----------------------------------------------
+INSERT INTO cart (cartNum, productNum, pInfoNum, productCount, username)
+VALUES(0, 1, 1, 1, 'admin');
+INSERT INTO cart (cartNum, productNum, pInfoNum, productCount, username)
+VALUES(0, 1, 2, 1, 'admin');
+INSERT INTO cart (cartNum, productNum, pInfoNum, productCount, username)
+VALUES(0, 2, 5, 1, 'admin');
+INSERT INTO cart (cartNum, productNum, pInfoNum, productCount, username)
+VALUES(0, 2, 6, 1, 'admin');
+INSERT INTO cart (cartNum, productNum, pInfoNum, productCount, username)
+VALUES(0, 3, 9, 1, 'admin');
+INSERT INTO cart (cartNum, productNum, pInfoNum, productCount, username)
+VALUES(0, 3, 10, 1, 'admin');
+
+---------------------------------------------------------- create table coupon ---------------------------------------------------------------------
+
+CREATE TABLE `fw01`.`coupon` (
+	`cuNum` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	`disRate` INT(11) NOT NULL,
+	`pubDate` DATE NOT NULL,
+	`exDate` DATE NOT NULL,
+	`cuName` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`cuNum`) USING BTREE,
+	INDEX `CU_UN_FK` (`username`) USING BTREE,
+	CONSTRAINT `CU_UN_FK` FOREIGN KEY (`username`) REFERENCES `fw01`.`member` (`username`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+------------------------------------- coupon 더미데이터 -----------------------------------------------
+
+INSERT INTO coupon (cuNum, username, disRate, pubDate, exDate, cuName)
+VALUES(0, 'admin', 0, NOW(), NOW(), 'null');
+INSERT INTO coupon (cuNum, username, disRate, pubDate, exDate, cuName)
+VALUES(0, 'admin', 10, NOW(), NOW(), 'test1');
+INSERT INTO coupon (cuNum, username, disRate, pubDate, exDate, cuName)
+VALUES(0, 'admin', 20, NOW(), NOW(), 'test2');
+INSERT INTO coupon (cuNum, username, disRate, pubDate, exDate, cuName)
+VALUES(0, 'admin', 30, NOW(), NOW(), 'test3');
+INSERT INTO coupon (cuNum, username, disRate, pubDate, exDate, cuName)
+VALUES(0, 'admin', 40, NOW(), NOW(), 'test4');
+
+---------------------------------------------------------- create table orderlist ---------------------------------------------------------------------
+
+CREATE TABLE `fw01`.`orderlist` (
+	`orderNum` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	`totPrice` BIGINT(20) NOT NULL,
+	`spPrice` BIGINT(20) NOT NULL,
+	`cuNum` BIGINT(20) NOT NULL,
+	`username` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	`destination` VARCHAR(450) NOT NULL DEFAULT '' COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`orderNum`) USING BTREE,
+	INDEX `OD_CN_FK` (`cuNum`) USING BTREE,
+	INDEX `OD_UN_FK` (`username`) USING BTREE,
+	CONSTRAINT `OD_CN_FK` FOREIGN KEY (`cuNum`) REFERENCES `fw01`.`coupon` (`cuNum`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `OD_UN_FK` FOREIGN KEY (`username`) REFERENCES `fw01`.`member` (`username`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+------------------------------------- orderlist 더미데이터 -----------------------------------------------
+
+INSERT INTO orderlist (orderNum, totPrice, spPrice, cuNum, username, destination)
+VALUES('123-123', 100000, 100000, 1, 'admin', 'test1');
+INSERT INTO orderlist (orderNum, totPrice, spPrice, cuNum, username, destination)
+VALUES('234-234', 10000, 9000, 1, 'admin', 'test2');
+
+---------------------------------------------------------- create table purchase ---------------------------------------------------------------------
+
+CREATE TABLE `fw01`.`purchase` (
+	`purNum` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`productNum` BIGINT(20) NOT NULL,
+	`pInfoNum` BIGINT(20) NOT NULL,
+	`orderNum` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	`productCount` INT(11) NOT NULL DEFAULT '0',
+	`proPriceSum` BIGINT(20) NOT NULL,
+	PRIMARY KEY (`purNum`) USING BTREE,
+	INDEX `PUR_PI_FK` (`pInfoNum`) USING BTREE,
+	INDEX `PUR_PN_FK` (`productNum`) USING BTREE,
+	INDEX `PUR_ON_FK` (`orderNum`) USING BTREE,
+	CONSTRAINT `PUR_ON_FK` FOREIGN KEY (`orderNum`) REFERENCES `fw01`.`orderlist` (`orderNum`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `PUR_PI_FK` FOREIGN KEY (`pInfoNum`) REFERENCES `fw01`.`productinfo` (`pInfoNum`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `PUR_PN_FK` FOREIGN KEY (`productNum`) REFERENCES `fw01`.`product` (`productNum`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+------------------------------------- purchase 더미데이터 -----------------------------------------------
+
+INSERT INTO purchase(purNum, productNum, pInfoNum, orderNum, productCount, proPriceSum)
+VALUES(0, 1, 1, '123-123', 2, 20000);
+INSERT INTO purchase(purNum, productNum, pInfoNum, orderNum, productCount, proPriceSum)
+VALUES(0, 2, 5, '123-123', 1, 20000);
+INSERT INTO purchase(purNum, productNum, pInfoNum, orderNum, productCount, proPriceSum)
+VALUES(0, 3, 9, '123-123', 2, 60000);
+INSERT INTO purchase(purNum, productNum, pInfoNum, orderNum, productCount, proPriceSum)
+VALUES(0, 1, 2, '234-234', 1, 10000);
+
+---------------------------------------------------------- create table mileage ---------------------------------------------------------------------
+
+CREATE TABLE `fw01`.`mileage` (
+	`mileNum` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`usedMile` BIGINT(20) NOT NULL,
+	`unableMile` BIGINT(20) NOT NULL,
+	`username` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	`orderNum` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	`mileContents` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	`enabledMile` BIGINT(20) NOT NULL,
+	PRIMARY KEY (`mileNum`) USING BTREE,
+	INDEX `MI_UN_FK` (`username`) USING BTREE,
+	INDEX `MI_ON_FK` (`orderNum`) USING BTREE,
+	CONSTRAINT `MI_ON_FK` FOREIGN KEY (`orderNum`) REFERENCES `fw01`.`orderlist` (`orderNum`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `MI_UN_FK` FOREIGN KEY (`username`) REFERENCES `fw01`.`member` (`username`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
+
+------------------------------------- mileage 더미데이터 -----------------------------------------------
+
+INSERT INTO mileage(mileNum, usedMile, unableMile, username, orderNum, mileContents, enabledMile)
+VALUES(0, 0, 0, 'admin', '123-123', 'testing', 0);
+INSERT INTO mileage(mileNum, usedMile, unableMile, username, orderNum, mileContents, enabledMile)
+VALUES(0, 1000, 0, 'admin', '234-234', 'testing', 0);
