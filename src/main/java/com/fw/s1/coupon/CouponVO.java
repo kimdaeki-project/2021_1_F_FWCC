c073461bec955c3068e7a56a08e0f7c50e44ea0b
rihee1214 <rihee1214@gmail.com>
2021-06-01 10:28:09 +0900

    에러해결용용

---
+++ src/main/java/com/fw/s1/coupon/CouponVO.java
@@ -0,0 +1,17 @@
+package com.fw.s1.coupon;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class CouponVO {
+
+	private Long cuNum;
+	private String username;
+	private Long cuSpNum;
+	private Date pubDate;
+	private Date exDate;
+	
+	private CouponspVO couponspVO;
+}
