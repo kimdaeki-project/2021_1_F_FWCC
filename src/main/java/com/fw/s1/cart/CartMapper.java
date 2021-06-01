afa2d4494190bfd23dbd9db52041fc1d2194b78e
Leepantam <nighfivego@gmail.com>
2021-06-01 09:03:59 +0900

    0601

--- src/main/java/com/fw/s1/cart/CartMapper.java
+++
@@ -1,8 +0,0 @@
-package com.fw.s1.cart;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CartMapper {
-
-}
--- src/main/java/com/fw/s1/cart/CartService.java
+++
@@ -1,8 +0,0 @@
-package com.fw.s1.cart;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CartService {
-
-}
