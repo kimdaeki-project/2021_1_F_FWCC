59e334ff48a948d72bbfb722c097c9a28fb9ed11
rihee1214 <rihee1214@gmail.com>
2021-06-30 19:42:35 +0900

    6/30 admin 수정
    admin 테이블은 구매한 개수를 각 제품 수만큼 체크해서 가져오는데 주문을 취소하더라도 이미 admin에 저장된 내역들은 남아있기때문에 해당 내역을 처리하기 위해 수정내역을 추가하였다.

--- src/main/java/com/fw/s1/admin/AdminMapper.java
+++ src/main/java/com/fw/s1/admin/AdminMapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.fw.s1.purchase.PurchaseVO;
+
 @Mapper
 public interface AdminMapper {
 
@@ -16,4 +18,8 @@ public interface AdminMapper {
 	public List<AdminVO> saleDays(HashMap<String, String> hashMap)throws Exception;
 	//해당 달에 팔린 내역을 가져온다.
 	public List<AdminVO> saleMonth(String date)throws Exception;
+	//주문을 취소했을 경우 해당 내역을 admin테이블에서 제거
+	public Long cancledOrder(List<PurchaseVO> list)throws Exception;
+	//취소한 내역을 복구할 경우 해당 내역을 admin테이블에 추가
+	public Long reCancledOrder(List<PurchaseVO> list)throws Exception;
 }
